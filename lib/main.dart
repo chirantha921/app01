826bd9224fbd7d895248e6b826e87c789a422b83
chirantha921 <chinthanarajamath@gmail.com>
2024-10-31 22:13:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: EsoftCard(),
+  ));
+}
+
+class EsoftCard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      appBar: AppBar(
+        title: const Text('Esoft ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[800],
+        elevation: 0.0,
+      ),
+    );
+
+
+
+  }
+
+
+
+
+      }
+
+
+
+
+
